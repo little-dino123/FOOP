470d92336f0a0b4cb6537a04be956451cabd39fe
davidZou <happydavid.zou@gmail.com>
2023-02-14 21:09:23 -0800

    add method to check total portfolio value

--- Unit_7/bankProject/StockPortfolio.py
+++ Unit_7/bankProject/StockPortfolio.py
@@ -7,22 +7,29 @@ class StockPortfolio:
         self.name = name
         self.accountName = accountName
         self.bankNum = bankNum
-        self.stocks = type(dict)
+        self.portfolio = type(dict)
         pass
 
     def buyStock(self, ticker, quantity):
         cost = StockMarket.ticker.price * quantity
         if cost <= self.accountName.currentBalance():
             BankAccount.self.accountName.balance -= cost
-            self.stocks[ticker] = quantity if ticker not in self.stocks else self.stocks[ticker] + quantity
+            self.portfolio[ticker] = quantity if ticker not in self.portfolio else self.portfolio[ticker] + quantity
             return "success"
         else:
             return "error"
 
     def sellStock(self, ticker, quantity):
-        if self.stocks[ticker] >= quantity:
-            self.stocks[ticker] -= quantity
+        if self.portfolio[ticker] >= quantity:
+            self.portfolio[ticker] -= quantity
             BankAccount.self.accountName.balance += StockMarket.ticker.price * quantity
             return "success"
         else:
             return "error"
+
+    def checkTotalValue(self):
+        total = 0
+        for stock in self.portfolio:
+            total += self.portfolio[stock]*StockMarket.stock.price
+        return total
+    
