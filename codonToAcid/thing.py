d9b5844cf5d77e6efca7df024a233655e53d8518
davidZou <happydavid.zou@gmail.com>
2023-02-14 16:42:22 -0800

    removed science thing

--- codonToAcid/thing.py
+++
@@ -1,6 +0,0 @@
-f=open("codonToAcid/thing.txt","w")
-for i in ["A","U","G","C"]:
-    for o in ["A","U","G","C"]:
-        for u in ["A","U","G","C"]:
-            f.write('"'+i+o+u+'":"",\n')
-f.close()
