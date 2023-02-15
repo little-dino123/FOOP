eb61df1cd3a3dde8739725db6f3f2f8d40a647ea
davidZou <happydavid.zou@gmail.com>
2023-02-14 16:39:08 -0800

    added unit 2 projects

---
+++ Unit_2/Calculator.py
@@ -0,0 +1,103 @@
+import tkinter as tk
+import math
+import random
+#init window
+main = tk.Tk()
+main.title("Four Function Calculator")
+main.geometry("300x300")
+#calculate function
+def calc():
+    #get the inputs from boxes
+    num1 = int(input1.get())
+    num2 = int(input2.get())
+    #get the operation to use
+    print(dropdown_var.get())
+    operation = dropdown_var.get()
+    #addition
+    if (operation == options[0]):
+        output.set(random.randint(0,12) if num1==1 and num2==1 else num1+num2)
+    #subtraction
+    elif (operation == options[1]):
+        output.set(num1-num2)
+    #multiplication
+    elif (operation == options[2]):
+        output.set(num1*num2)
+    #division
+    elif (operation == options[3]):
+        output.set(num1/num2)
+    #exponents
+    elif (operation == options[4]):
+        output.set(num1**num2)
+    #square root
+    elif (operation == options[5]):
+        output.set(math.sqrt(num1))
+    #sine
+    elif (operation == options[6]):
+        output.set(math.sin(num1))
+    #cosine
+    elif (operation == options[7]):
+        output.set(math.cos(num1))
+    #tangent
+    elif (operation == options[8]):
+        output.set(math.tan(num1))
+    #factorial
+    elif (operation == options[9]):
+        output.set(math.factorial(num1))
+    #set as error if its a error
+    else:
+        output.set("error")
+#disables the 2nd input box when any of the functions are selected as they only need 1 input
+def enOrDisable():
+    if (dropdown_var.get() in options_functions):
+        input2.config(state = "disabled")
+    else:
+        input2.config(state = "normal")
+#the list of operations that could be selected
+options = [
+    "+",
+    "−",
+    "×",
+    "÷",
+    "xʸ",
+    "√",
+    "Sin",
+    "Cos",
+    "Tan",
+    "!"
+]
+#list of funtions for the disabling
+options_functions = [
+    "√",
+    "Sin",
+    "Cos",
+    "Tan",
+    "!"
+]
+#creates frame because without it the widgets are vertically centered
+frame = tk.Frame()
+frame.pack()
+#creates the variable that stores what is selected in the dropdown
+dropdown_var = tk.StringVar(main)
+dropdown_var.set(options[0])
+#creates entry box 1 and 2, then set default value as 0 to not cause errors
+input1 = tk.Entry(frame, width = 10)
+input2 = tk.Entry(frame, width = 10)
+input1.insert(0, 0)
+input2.insert(0, 0)
+#creates dropdown menu with list of operations, calls    
+dropdown = tk.OptionMenu(frame, dropdown_var, *options, command = enOrDisable())
+#basically the calculate button because im too lazy to do the trace thing that automatically calls func calc
+equals = tk.Button(frame, text = "=", command = calc)
+#variable to store the output
+output = tk.StringVar(main)
+output.set(0)
+#label to show the output
+result = tk.Label(frame, textvar = output)
+#packs all widgets
+input1.pack(side = tk.LEFT)
+dropdown.pack(side = tk.LEFT)
+input2.pack(side = tk.LEFT)
+equals.pack(side = tk.LEFT)
+result.pack(side = tk.LEFT)
+#render
+tk.mainloop()
---
+++ Unit_2/RPSLS.py
@@ -0,0 +1,115 @@
+# library imports
+# import RNG
+import secrets
+# import GUI
+import tkinter as tk
+
+# tkinter window init
+# create GUI window
+window = tk.Tk()
+# set window title
+window.title("Rock Paper Scissors Lizard Spock")
+# set window size
+window.geometry("500x100")
+
+# function for making buttons in a single line later
+def make_button(master, but_name, but_command, but_text):
+  #makes the button using arguments
+  but_name = tk.Button(master, text = but_text, command = but_command)
+  #pack button
+  but_name.pack(side = tk.LEFT)
+
+
+# all  calculations and logic for the game
+def play(entry):
+  # generates a random number from 1-5 inclusive
+  comp = (secrets.randbelow(4)+1)
+
+  # assigns a string to a variable for each computer choice for display later on
+  if (comp == 1):
+    comp_play = "Rock"
+  elif (comp == 2):
+    comp_play = "Paper"
+  elif (comp == 3):
+    comp_play = "Scissor"
+  elif (comp == 4):
+    comp_play = "Lizard"
+  elif (comp == 5):
+    comp_play = "Spock"
+  else:
+    comp_play = "Error"
+
+  # assigns a string to a variable for each player choice for display later on
+  if (entry == 1):
+    player_play = "Rock"
+  elif (entry == 2):
+    player_play = "Paper"
+  elif (entry == 3):
+    player_play = "Scissor"
+  elif (entry == 4):
+    player_play = "Lizard"
+  elif (entry == 5):
+    player_play = "Spock"
+  else:
+    player_play = "Error"
+  # prints some stuff for debugging if needed
+  print("Input:", entry)
+  print("Computer RNG:", comp)
+  # decides if player wins or looses using equation given
+  # also sets text for display
+  w_l_var = (comp-entry+5)%5
+  print("Win or Lose:", w_l_var)
+  if (w_l_var == 1 or w_l_var == 2):
+    w_l_text = "You Win!"
+    print("Win")
+  elif(w_l_var == 0):
+    w_l_text = "It's a Tie!"
+    print("Tie")
+  elif(w_l_var == 3 or w_l_var == 4):
+    w_l_text = "You Lose!"
+    print("Lose")
+  else:
+    print("Error")
+  # temporary varibale beccause .set doesn't allow multiple arguments
+  # use + because there's weird parenthese with commas
+  w_l_temp = 'You Played '+ player_play + '. The Computer Plays '+ comp_play + ". " + w_l_text
+  # prints string for debugging purposes
+  print(w_l_temp)
+  # sets the stringvar display text
+  w_l.set(w_l_temp)
+
+# displays text at the top
+# creates frame
+frame_w_l = tk.Frame()
+# creates text variable
+w_l = tk.StringVar(master = frame_w_l)
+# sets above textvar
+w_l.set("Choose a Play!")
+# creates label that shows the text
+label_w_l = tk.Label(master = frame_w_l, textvariable = w_l)
+# pack label
+label_w_l.pack()
+# pack frame
+frame_w_l.pack()
+
+# buttons
+# frame for buttons
+frame_button = tk.Frame()
+# button creation using function
+# rock button
+make_button(frame_button, "button_rock", lambda: play(1), "Rock")
+# paper button
+make_button(frame_button, "button_paper", lambda: play(2), "Paper")
+# scissor button
+make_button(frame_button, "button_scissor", lambda: play(3), "Scissor")
+# lizard button
+make_button(frame_button, "button_lizard", lambda: play(4), "Lizard")
+# spock button
+make_button(frame_button, "button_spock", lambda: play(5), "Spock")
+# pack button frame
+frame_button.pack()
+
+
+
+#Renders GUI
+tk.mainloop()
