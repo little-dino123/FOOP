eb12fcc04fc5208cd9d829bb77c151815fb4e55f
davidZou <happydavid.zou@gmail.com>
2023-02-14 20:56:19 -0800

    add unit 6 thing

--- Unit_7/bankProject/StockPortfolio.py
+++ Unit_7/bankProject/StockPortfolio.py
@@ -1,13 +1,28 @@
+import BankAccount
+import StockMarket
+
+
 class StockPortfolio:
     def __init__(self, name, accountName, bankNum):
         self.name = name
         self.accountName = accountName
         self.bankNum = bankNum
-        self.stocks= type(dict)
+        self.stocks = type(dict)
         pass
 
-    def buyStock(self, name, ticker, quantity):
-        if ticker.price*quantity <= self.accountName.currentBalance():
-            pass
+    def buyStock(self, ticker, quantity):
+        cost = StockMarket.ticker.price * quantity
+        if cost <= self.accountName.currentBalance():
+            BankAccount.self.accountName.balance -= cost
+            self.stocks[ticker] = quantity if ticker not in self.stocks else self.stocks[ticker] + quantity
+            return "success"
         else:
-            return "Error, not enough money"
+            return "error"
+
+    def sellStock(self, ticker, quantity):
+        if self.stocks[ticker] >= quantity:
+            self.stocks[ticker] -= quantity
+            BankAccount.self.accountName.balance += StockMarket.ticker.price * quantity
+            return "success"
+        else:
+            return "error"
