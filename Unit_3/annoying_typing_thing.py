ab26b6f3cf8e11b133cb3cdd45b27338779c9339
davidZou <happydavid.zou@gmail.com>
2023-02-21 11:32:39 -0800

    removed some extra files

--- Unit_3/annoying_typing_thing.py
+++
@@ -1,5 +0,0 @@
-def thing(str):
-    out = ""
-    for var in str.split(): out+=var.capitalize()+" "
-    return out
-print(thing("if you do this ur a  dumbass"))
--- Unit_3/sarcasm.py
+++
@@ -1,6 +0,0 @@
-from random import *
-def s(input):
-    output = ""
-    for var in input: output += var.upper() if randint(0,2)==1 else var.lower()
-    return output
-print(s("omg your personality matters more than your grade!!!!!!!!!!! this is why we are getting rid of math lanes and skip tests!!!!!!!!!!!!!!!"))
