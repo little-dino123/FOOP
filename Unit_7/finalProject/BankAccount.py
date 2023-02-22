2b4ca3e8bf42c1b008f17ee161014b85e87702e8
davidZou <happydavid.zou@gmail.com>
2023-02-22 14:10:02 -0800

    added final project folder

---
+++ Unit_7/finalProject/BankAccount.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, balance, name, number):
+        self.interest = 0.5
+        self.balance = balance
+        self.name = name
+        self.number = number
+
+    def changeInterestRate(self, newRate):
+        self.interest = newRate
+        return "success"
+
+    def deposit(self, cash):
+        self.balance += cash
+        return "success"
+
+    def withdraw(self, cash):
+        if cash <= self.balance:
+            self.balance -= cash
+            return "success"
+        return "error"
+
+    def currentBalance(self):
+        return self.balance
+
---
+++ Unit_7/finalProject/main.py
@@ -0,0 +1,2 @@
+from BankAccount import BankAccount
+myBankAccount = BankAccount(10000000, "david", 123456789)
