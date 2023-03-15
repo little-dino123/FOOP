1ce36ce62983d28874316e006725017415572581
davidZou <happydavid.zou@gmail.com>
2023-03-15 14:38:06 -0700

    moooooooooooooooooooooooooooooooooooooooooooooooooo

--- java/unit8/Cow/Main.java
+++ java/unit8/Cow/Main.java
@@ -3,6 +3,7 @@ package unit8.Cow;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("the cow says mooooooooo");
 		CowFrames c = new CowFrames();
 		c.setVisible(true);
 	}
