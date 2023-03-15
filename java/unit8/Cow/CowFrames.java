c1b97bbde1430b6fa5fabefb69aef37babf803a4
davidZou <happydavid.zou@gmail.com>
2023-03-15 14:01:21 -0700

    init cow project kekw

--- java/unit8/Cow/CowFrames.java
+++ java/unit8/Cow/CowFrames.java
@@ -1,27 +1,36 @@
+package unit8.Cow;
+
 import java.awt.*;
 import javax.swing.JFrame;
 
 public class CowFrames extends JFrame {
 	private final Color BISQUE = new Color(0xcdb79e);
-	private final Color CHARTRUSE = new Color(0x7FFF00);
+	private final Color GRASS = new Color(0x7ec236);
+	private final int OFFSET = -300;
+
 	public CowFrames () {
 		init();
 	}
 	
 	public void init() {
-		setSize(700,600);
-		setBackground(Color.BLUE);
+		setSize(350,200);
+		setBackground(GRASS);
 		repaint();
 	}
-	
+	public void drawLegs(Graphics g){
+		g.setColor(Color.BLACK);
+		g.fillRect(400+OFFSET,140,10,50);
+		g.fillRect(490+OFFSET,140,10,50);
+
+	}
 	public void paint(Graphics g) {
-    g.setColor(BISQUE);
-		//g.setColor(CHARTRUSE);
-		g.fillRect(0, 0, 400, 200);
-		g.setColor(Color.ORANGE);
-		g.drawOval(350, 50, 200, 100);
-    g.setColor(Color.BLACK);
-    g.fillOval(355, 55, 25, 60);
+		g.setColor(BISQUE);
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillOval(350+OFFSET, 50, 200, 100);
+		g.setColor(Color.WHITE);
+		g.fillOval(340+OFFSET, 50, 60, 40);
+		drawLegs(g);
+
 
 
     //advanced
--- java/unit8/Cow/Main.java
+++ java/unit8/Cow/Main.java
@@ -1,3 +1,5 @@
+package unit8.Cow;
+
 public class Main {
 
 	public static void main(String[] args) {
