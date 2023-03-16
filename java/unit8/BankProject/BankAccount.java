bdf70b62cb3d9ce719ccd0168f708ed0bbfebd4f
davidZou <happydavid.zou@gmail.com>
2023-03-15 21:34:39 -0700

    init Java version of bank account project

---
+++ java/unit8/BankProject/BankAccount.java
@@ -0,0 +1,9 @@
+package unit8.BankProject;
+
+public class BankAccount {
+    public BankAccount(float balance, String name, String accountNumber, double apr){
+    }
+    public void depositIntoAccount(float depositAmount){
+
+    }
+}
--- python/unit7/finalProject/inventory.py
+++ python/unit7/finalProject/inventory.py
@@ -1,4 +1,5 @@
 class Inventory:
-    def __init__(self, name):
+    def __init__(self, name, items):
         self.name = name
+        self.items = items
     
