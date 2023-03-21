7c6b7160310a8eee2f445d419af2d0b87bb6f98d
davidZou <happydavid.zou@gmail.com>
2023-03-20 23:44:25 -0700

    added door handle, fixed some wierd logic

--- java/unit8/JavaTurtleDefault/Main.java
+++ java/unit8/JavaTurtleDefault/Main.java
@@ -1,16 +1,14 @@
 package unit8.JavaTurtleDefault;
 
-import javax.swing.*;
-import java.awt.event.*;
-import java.util.*;
 import java.awt.*;
-import java.math.*;
-
 public class Main {
     public static final int canvasW = 500;
     public static final int canvasH = 500;
     public static final int houseW = 200;
     public static final int houseH = 150;
+    public static final int houseX = canvasW/2;
+    public static final int houseY = canvasH/2 + 100;
+    public static final int ground = houseY + houseH / 2;
 
 
     public static void main(String[] args) {
@@ -19,14 +17,12 @@ public class Main {
         Turtle t = new Turtle(world);
         t.hide();
         t.setPenColor(Color.BLACK);
-//        drawHouse(canvasW / 2, canvasH / 2, houseW, houseH, t);
-        circ(canvasW / 2, canvasH / 2, 100, t);
+        drawHouse(houseX, houseY, houseW, houseH, t);
     }
 
     public static void drawHouse(int x, int y, int w, int h, Turtle t) {
-        final int houseY = y;
         drawBody(x, y, w, h, t);
-        drawRoof(x, y - h / 2, (int) (w * 1.5), (int) (h * 0.5), t);
+        drawRoof(x, y - h / 2, (int) (w*5/3), (int) (h*2/3), t);
         drawWindow((int) (0.8 * x), (int) (0.95 * y), 50, 40, t);
         drawDoor((int) (1.2 * x), y, (int) (w / 3), (int) (h / 1.5), t);
     }
@@ -58,9 +54,9 @@ public class Main {
     }
 
     public static void drawDoor(int x, int y, int w, int h, Turtle t) {
-        y = (int) ((canvasW / 2) + (houseH / 2) - (h / 2));
+        y = (int) (ground - (h / 2));
         rect(x, y, w, h, t);
-        circ(x, y, 100, t);
+        circ(x-h/5, (int)(y*0.975), 5, t);
     }
 
     public static void rect(int x, int y, int w, int h, Turtle t) {
@@ -80,7 +76,7 @@ public class Main {
 
     public static void circ(int x, int y, int r, Turtle t) {
         t.penUp();
-        t.moveTo(x, y - r);
+        t.moveTo(x, y + r);
         t.penDown();
         for (int i = 0; i <= 360; i += 5) {
             double circX = r * Math.sin(Math.toRadians(i));
@@ -88,6 +84,5 @@ public class Main {
             t.moveTo((int) (x + circX), (int) (y + circY));
         }
         t.penUp();
-        t.show();
     }
 }
