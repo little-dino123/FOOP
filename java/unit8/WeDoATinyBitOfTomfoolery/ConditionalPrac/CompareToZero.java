ab93042c57e6d8e461a64086c8298aa849fc2143
davidZou <happydavid.zou@gmail.com>
2023-03-29 14:13:38 -0700

    init conditional prac

--- java/unit8/WeDoATinyBitOfTomfoolery/ConditionalPrac/CompareToZero.java
+++ java/unit8/WeDoATinyBitOfTomfoolery/ConditionalPrac/CompareToZero.java
@@ -1,2 +1,22 @@
-package unit8.WeDoATinyBitOfTomfoolery.ConditionalPrac;public class CompareToZero {
+package unit8.WeDoATinyBitOfTomfoolery.ConditionalPrac;
+
+import java.util.Scanner;
+
+public class CompareToZero {
+
+    public static void main(String[] args) {
+        double x;
+        Scanner s  = new Scanner(System.in);
+        System.out.println("enter a numbr idot");
+        try {
+            x = s.nextDouble();
+            System.out.println(cZero(x));
+        }
+        catch (Exception InputMismatchException) {
+            System.out.println("idot enter a numbr not letters stooppid");
+        }
+    }
+    public static String cZero(double x){
+        return x > 0 ? "ur num is beeger than 0" : x < 0 ? "your num is less than 0" : "ur num is 0";
+    }
 }
--- java/unit8/WeDoATinyBitOfTomfoolery/ConditionalPrac/Magic8Ball.java
+++ java/unit8/WeDoATinyBitOfTomfoolery/ConditionalPrac/Magic8Ball.java
@@ -1,2 +1,18 @@
-package unit8.WeDoATinyBitOfTomfoolery.ConditionalPrac;public class Magic8Ball {
+package unit8.WeDoATinyBitOfTomfoolery.ConditionalPrac;
+
+import java.util.Scanner;
+
+public class Magic8Ball {
+    private static String[] l = new String[]{"yes", "sure ig idk", "ur bad kid", "no", "maybe", "skill issue", "cope", "ratio"};
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("ask a question");
+        s.next();
+        System.out.println(returnRandom());
+
+    }
+    public static String returnRandom(){
+        return l[(int)(Math.random()*l.length)];
+    }
 }
