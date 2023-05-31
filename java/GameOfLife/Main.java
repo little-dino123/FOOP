5eea4824d56164cded81806c6fcf62c5cb9f6660
davidZou <happydavid.zou@gmail.com>
2023-05-30 18:20:05 -0700

    Revert "updated some stuff, made it easier to use, added border"
    This reverts commit c2fbbde8

--- java/GameOfLife/Main.java
+++ java/GameOfLife/Main.java
@@ -1,16 +1,13 @@
 package GameOfLife;
-
 import org.apache.commons.lang3.StringUtils;
-
 public class Main {
-    public static Board board = new Board(Config.height, Config.width, Config.initBoard);
-
+    public static Board board = new Board(Config.height,Config.width,Config.initBoard);
     public static void main(String[] args) throws InterruptedException {
-        while (true) {
+        while(true){
             board.updateAllBoards();
             String[][] displayBoard = board.getDisplayBoard();
             StringBuilder output = new StringBuilder();
-            output.append(StringUtils.repeat("_", displayBoard[0].length * 2));
+            output.append(StringUtils.repeat("_", displayBoard[0].length*2));
             for (String[] line : displayBoard) {
                 output.append("|");
                 for (String s : line) {
@@ -18,7 +15,7 @@ public class Main {
                 }
                 output.append("|\n");
             }
-            output.append(StringUtils.repeat("_", displayBoard[0].length * 2));
+            output.append(StringUtils.repeat("_", displayBoard[0].length*2));
 
             System.out.print("\033[H\033[2J");
             System.out.flush();
